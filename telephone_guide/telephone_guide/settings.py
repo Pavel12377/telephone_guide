bcc1f0f763bd808c9be4b64ecfb8e0102f16766e
Pavel12377 <pstar_88@mail.ru>
2024-06-06 15:52:34 +0300

    fill gitignore, remove sekret key

--- telephone_guide/telephone_guide/settings.py
+++ telephone_guide/telephone_guide/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zqlt2v-n)u27$l#u&avd@^yw09xlunb$!@38mbj%(6w25ab+$v'
-
+SECRET_KEY = ''
+#django-insecure-zqlt2v-n)u27$l#u&avd@^yw09xlunb$!@38mbj%(6w25ab+$v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
